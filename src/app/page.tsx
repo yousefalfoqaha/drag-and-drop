6a67f44251e659f2900345ef3660fce024fa9107
yousefalfoqaha <yousefmustafaadam@gmail.com>
2024-09-19 19:17:24 +0300

    refactored data to become map structure for better rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,56 @@
 'use client'
+
 import { Button } from '@/components/ui/button'
-import { ScrollArea } from '@/components/ui/scroll-area'
-import { Plus } from 'lucide-react'
 import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
-import {
-  studyPlan,
   courses,
-  sections,
   initialFlowsheet,
-  years,
-  semesters,
+  Year,
+  Semester,
+  Flowsheet,
 } from '@/lib/data'
+import { Plus } from 'lucide-react'
 import { useState } from 'react'
+import { useImmer, Updater } from 'use-immer'
 
 const App = () => {
+  const [flowsheet, updateFlowsheet] = useImmer(initialFlowsheet)
+
   return (
     <div className="flex p-12 h-screen w-full">
-      <Flowsheet />
+      <Flowsheet
+        name={flowsheet.name}
+        years={flowsheet.years}
+        updateFlowsheet={updateFlowsheet}
+      />
     </div>
   )
 }
 
 export default App
 
-const Flowsheet = () => {
-  const [flowsheet, setFlowsheet] = useState(initialFlowsheet)
+type flowsheetProps = {
+  name: string
+  years: Year[]
+  updateFlowsheet: Updater<Flowsheet>
+}
 
+const Flowsheet = ({ name, years, updateFlowsheet }: flowsheetProps) => {
   return (
     <main>
-      <header className="text-2xl font-semibold py-2">{flowsheet.name}</header>
+      <header className="text-2xl font-semibold py-2">{name}</header>
       <div className="flex gap-1">
-        {flowsheet.yearIds.map((id) => (
-          <Year key={id} id={id} />
-        ))}
+        {years.map((year) => {
+          if (!year) return null
+          return (
+            <Year
+              key={year.id}
+              id={year.id}
+              name={year.name}
+              semesters={year.semesters}
+              updateFlowsheet={updateFlowsheet}
+            />
+          )
+        })}
       </div>
     </main>
   )
@@ -49,21 +58,31 @@ const Flowsheet = () => {
 
 interface yearProps {
   id: number
+  name: string
+  semesters: Semester[]
+  updateFlowsheet: Updater<Flowsheet>
 }
 
-const Year = ({ id }: yearProps) => {
-  const year = years.find((year) => year.id === id)
-  if (!year) return null
-
+const Year = ({ id, name, semesters, updateFlowsheet }: yearProps) => {
   return (
     <section>
       <p className="text-center font-semibold p-2 border rounded-lg border-neutral-400">
-        {year.name}
+        {name}
       </p>
       <div className="flex gap-1 my-1">
-        {year.semesterIds.map((id) => (
-          <Semester key={id} id={id} />
-        ))}
+        {semesters.map((semester) => {
+          if (!semester) return null
+          return (
+            <Semester
+              key={semester.id}
+              id={semester.id}
+              name={semester.name}
+              courseIds={semester.courseIds}
+              yearId={id}
+              updateFlowsheet={updateFlowsheet}
+            />
+          )
+        })}
       </div>
     </section>
   )
@@ -71,137 +90,75 @@ const Year = ({ id }: yearProps) => {
 
 interface semesterProps {
   id: number
+  name: string
+  courseIds: number[]
+  yearId: number
+  updateFlowsheet: Updater<Flowsheet>
 }
 
-const Semester = ({ id }: semesterProps) => {
-  const semester = semesters.find((semester) => semester.id === id)
-  if (!semester) return null
-  // const totalCreditHours = courses.reduce(
-  //   (total, course) => total + course.creditHours,
+const Semester = ({
+  id,
+  name,
+  courseIds,
+  yearId,
+  updateFlowsheet,
+}: semesterProps) => {
+  // const totalCreditHours = course.reduce(
+  //   (total, course) => total + (course?.creditHours || 0),
   //   0
   // )
 
+  const handleAddCourse = () => {
+    updateFlowsheet((draft) => {
+      draft.years.semesters[id].courseIds.push(3)
+    })
+  }
+
   return (
-    <>
-      <section className="flex flex-col gap-1 w-36">
-        <header className="text-center">
-          <h1>{semester.name}</h1>
-          {/* <p className="font-semibold text-xs text-neutral-500">
-            {totalCreditHours} Cr Hr
-          </p> */}
-        </header>
-        {semester.courseIds.map((id) => (
-          <CourseCard key={id} id={id} />
-        ))}
-      </section>
-    </>
+    <section className="flex flex-col gap-1 w-36">
+      <header className="text-center">
+        <h1>{name}</h1>
+        {/* <p className="font-semibold text-muted-foreground text-sm">
+          {totalCreditHours} Cr Hr
+        </p> */}
+      </header>
+      {courseIds.map((id) => {
+        const course = courses[id]
+        if (!course) return null
+        return (
+          <CourseCard
+            key={course.id}
+            code={course.code}
+            name={course.name}
+            creditHours={course.creditHours}
+          />
+        )
+      })}
+      <Button
+        onClick={handleAddCourse}
+        variant="ghost"
+        className="hover:shadow transition-all"
+      >
+        <Plus className="scale-75" />
+      </Button>
+    </section>
   )
 }
 
-// interface studyPlanProps extends studyPlanData {
-//   setFlowsheet: React.Dispatch<React.SetStateAction<flowsheetData>>
-//   selectedSemester: semesterData
-// }
-
-// const StudyPlan = ({
-//   name,
-//   sections,
-//   selectedSemester,
-//   setFlowsheet,
-// }: studyPlanProps) => {
-//   const handleAddCourse = (courseId: number) => {
-//     setFlowsheet((prevFlowsheet) => {
-//       const updatedSemesters = prevFlowsheet.semesters.map((semester) =>
-//         semester.id === selectedSemester.id
-//           ? { ...semester, courseIds: [...semester.courseIds, courseId] }
-//           : semester
-//       )
-
-//       return {
-//         ...prevFlowsheet,
-//         semesters: updatedSemesters,
-//       }
-//     })
-//   }
-
-//   return (
-//     <Dialog>
-//       <DialogTrigger asChild>
-//         <Button
-//           variant="ghost"
-//           className="w-full hover:h-36 transition-all hover:border border-neutral-400"
-//         >
-//           <Plus className="transition-transform text-neutral-700 duration-300 scale-75" />
-//         </Button>
-//       </DialogTrigger>
-//       <DialogContent>
-//         <DialogHeader>
-//           <DialogTitle>{name}</DialogTitle>
-//           <DialogDescription>
-//             Add available courses to {selectedSemester.name.toLowerCase()}{' '}
-//             semester
-//           </DialogDescription>
-//         </DialogHeader>
-//         <section>
-//           <ScrollArea>
-//             <div className="grid grid-cols-3 gap-1 h-72">
-//               {sections.map((section) =>
-//                 section.courses.map((course) => (
-//                   <Course
-//                     onClick={() => handleAddCourse(course.id)}
-//                     key={course.id}
-//                     {...course}
-//                   />
-//                 ))
-//               )}
-//             </div>
-//           </ScrollArea>
-//         </section>
-//         Courses selected: 0
-//         <DialogFooter>
-//           <DialogClose asChild>
-//             <Button type="submit">Add Courses</Button>
-//           </DialogClose>
-//         </DialogFooter>
-//       </DialogContent>
-//     </Dialog>
-//   )
-// }
-
 interface courseCardProps {
-  id: number
+  code: string
+  name: string
+  creditHours: number
 }
 
-const CourseCard = ({ id }: courseCardProps) => {
-  const course = courses.find((course) => course.id === id)
-  if (!course) return null
-
+const CourseCard = ({ code, name, creditHours }: courseCardProps) => {
   return (
-    <div className="p-2 h-36 w-36 transition-all border border-neutral-400 shadow-md bg-neutral-200 rounded-md flex flex-col hover:bg-purple-500/30">
-      <header className="font-semibold">{course.code}</header>
-      <p className="line-clamp-3">{course.name}</p>
+    <div className="p-2 h-36 w-36 transition-all border border-neutral-200 hover:shadow rounded-md flex flex-col hover:border-none hover:bg-purple-200/30">
+      <header className="font-semibold">{code}</header>
+      <p className="line-clamp-3">{name}</p>
       <footer className="text-right mt-auto text-neutral-500">
-        {course.creditHours} Cr Hr
+        {creditHours} Cr Hr
       </footer>
     </div>
   )
 }
-
-// interface courseProps extends courseData {
-//   onClick: any
-// }
-
-// const Course = ({ id, code, name, creditHours, onClick }: courseProps) => {
-//   return (
-//     <div
-//       onClick={onClick}
-//       className="p-2 h-36 w-36 transition-all border border-neutral-400 shadow-md bg-neutral-200 rounded-md flex flex-col hover:bg-purple-500/30"
-//     >
-//       <header className="font-semibold">{code}</header>
-//       <p className="line-clamp-3">{name}</p>
-//       <footer className="text-right mt-auto text-neutral-500">
-//         {creditHours} Cr Hr
-//       </footer>
-//     </div>
-//   )
-// }
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,358 +1,386 @@
-export type studyPlanData = {
+export interface StudyPlan {
   id: number
   name: string
-  sectionIds: number[]
+  sections: Section[]
 }
 
-export type sectionData = {
+export interface Section {
   id: number
   name: string
   requiredCreditHours: number
   courseIds: number[]
 }
 
-export type courseData = {
+export interface Course {
   id: number
   code: string
   name: string
   creditHours: number
 }
 
-export type flowsheetData = {
+export interface Flowsheet {
   id: number
   name: string
-  yearIds: number[]
+  years: { [key: number]: Year }
 }
 
-export type yearData = {
+export interface Year {
   id: number
   name: string
-  semesterIds: number[]
+  semesters: { [key: number]: Semester }
 }
 
-export type semesterData = {
+export interface Semester {
   id: number
   name: string
   courseIds: number[]
 }
 
-export const studyPlan: studyPlanData = {
+export const studyPlan: StudyPlan = {
   id: 1,
   name: 'Computer Science 2023/2024 - General Track',
-  sectionIds: [1, 2, 3, 4],
-}
-
-export const courses: courseData[] = [
+  sections: [
     {
       id: 1,
-      code: 'ARB100',
-      name: 'Arabic',
-      creditHours: 3,
+      name: 'University Requirements',
+      requiredCreditHours: 16,
+      courseIds: [1, 2, 3, 4, 5, 6, 7, 8, 9],
     },
     {
       id: 2,
-      code: 'ENGL1001',
-      name: 'Upper-Intermediate English',
-      creditHours: 3,
+      name: 'School Requirements',
+      requiredCreditHours: 21,
+      courseIds: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
     },
     {
       id: 3,
-      code: 'ENGL1002',
-      name: 'Advanced English',
-      creditHours: 3,
+      name: 'Program Requirements',
+      requiredCreditHours: 86,
+      courseIds: [
+        23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45,
+      ],
     },
     {
       id: 4,
-      code: 'GERL101B1',
-      name: 'German I B1-Track',
-      creditHours: 3,
+      name: 'Special Program Requirements (General Track)',
+      requiredCreditHours: 12,
+      courseIds: [46, 47, 48, 49],
     },
-    {
-      id: 5,
-      code: 'GERL102B1',
-      name: 'German II B1-Track',
-      creditHours: 3,
-    },
-    {
-      id: 6,
-      code: 'GERL102B2',
-      name: 'German II B2-Track',
-      creditHours: 3,
-    },
-    {
-      id: 7,
-      code: 'MILS100',
-      name: 'Military Science',
-      creditHours: 3,
-    },
-    {
-      id: 8,
-      code: 'NE101',
-      name: 'National Education',
-      creditHours: 3,
-    },
-    {
-      id: 9,
-      code: 'NEE101',
-      name: 'National Education in English',
-      creditHours: 3,
-    },
-    {
-      id: 10,
-      code: 'GERL201B1',
-      name: 'German III B1-Track',
-      creditHours: 3,
-    },
-    {
-      id: 11,
-      code: 'GERL201B2',
-      name: 'German III B2-Track',
-      creditHours: 3,
-    },
-    {
-      id: 12,
-      code: 'GERL202B1',
-      name: 'German IV B1-Track',
-      creditHours: 3,
-    },
-    {
-      id: 13,
-      code: 'GERL202B2',
-      name: 'German IV B2-Track',
-      creditHours: 3,
-    },
-    {
-      id: 14,
-      code: 'MATH101',
-      name: 'Calculus I',
-      creditHours: 3,
-    },
-    {
-      id: 15,
-      code: 'MATH102',
-      name: 'Calculus II',
-      creditHours: 3,
-    },
-    {
-      id: 16,
-      code: 'CS116',
-      name: 'Computing Fundamentals',
-      creditHours: 3,
-    },
-    {
-      id: 17,
-      code: 'CS1160',
-      name: 'Computing Fundamentals lab',
-      creditHours: 1,
-    },
-    {
-      id: 18,
-      code: 'CS117',
-      name: 'Object-Oriented Programming',
-      creditHours: 3,
-    },
-    {
-      id: 19,
-      code: 'CS1170',
-      name: 'Object-Oriented Programming Lab',
-      creditHours: 1,
-    },
-    {
-      id: 20,
-      code: 'CE212',
-      name: 'Digital Systems',
-      creditHours: 3,
-    },
-    {
-      id: 21,
-      code: 'CE2120',
-      name: 'Digital Systems Lab',
-      creditHours: 1,
-    },
-    {
-      id: 22,
-      code: 'EE317',
-      name: 'Linear Algebra',
-      creditHours: 3,
-    },
-    {
-      id: 23,
-      code: 'GERL301B1',
-      name: 'German V B1-Track',
-      creditHours: 3,
-    },
-    {
-      id: 24,
-      code: 'GERL301B2',
-      name: 'German V B2-Track',
-      creditHours: 3,
-    },
-    {
-      id: 25,
-      code: 'GERL302B1',
-      name: 'German VI B1-Track',
-      creditHours: 3,
-    },
-    {
-      id: 26,
-      code: 'GERL302B2',
-      name: 'German VI B2-Track',
-      creditHours: 3,
-    },
-    {
-      id: 27,
-      code: 'IE0121',
-      name: 'Probability and Statistics',
-      creditHours: 3,
-    },
-    {
-      id: 28,
-      code: 'CS201',
-      name: 'Discrete Structures',
-      creditHours: 3,
-    },
-    {
-      id: 29,
-      code: 'CE201',
-      name: 'Computer Architecture and Organization',
-      creditHours: 3,
-    },
-    {
-      id: 30,
-      code: 'CS222',
-      name: 'Theory of Algorithms',
-      creditHours: 3,
-    },
-    {
-      id: 31,
-      code: 'CS223',
-      name: 'Data Structures',
-      creditHours: 3,
-    },
-    {
-      id: 32,
-      code: 'CS264',
-      name: 'Visual Programming',
-      creditHours: 3,
-    },
-    {
-      id: 33,
-      code: 'CS263',
-      name: 'Database Management Systems',
-      creditHours: 3,
-    },
-    {
-      id: 34,
-      code: 'CS323',
-      name: 'Computational Theory',
-      creditHours: 3,
-    },
-    {
-      id: 35,
-      code: 'CS342',
-      name: 'Software Engineering',
-      creditHours: 3,
-    },
-    {
-      id: 36,
-      code: 'CE352',
-      name: 'Computer Networks',
-      creditHours: 3,
-    },
-    {
-      id: 37,
-      code: 'CS355',
-      name: 'Web Technologies',
-      creditHours: 3,
-    },
-    {
-      id: 38,
-      code: 'CS356',
-      name: 'Information Security',
-      creditHours: 3,
-    },
-    {
-      id: 39,
-      code: 'CE357',
-      name: 'Operating Systems',
-      creditHours: 3,
-    },
-    {
-      id: 40,
-      code: 'CE3570',
-      name: 'Operating Systems Lab',
-      creditHours: 1,
-    },
-    {
-      id: 41,
-      code: 'CS391',
-      name: 'Field Training',
-      creditHours: 0,
-    },
-    {
-      id: 42,
-      code: 'CS416',
-      name: 'Systems Programming',
-      creditHours: 3,
-    },
-    {
-      id: 43,
-      code: 'CS451',
-      name: 'Artificial Intelligence',
-      creditHours: 3,
-    },
-    {
-      id: 44,
-      code: 'CS491',
-      name: 'International Internship 20 weeks',
-      creditHours: 12,
-    },
-    {
-      id: 45,
-      code: 'CS492',
-      name: 'Senior Project',
-      creditHours: 3,
-    },
-    {
-      id: 46,
-      code: 'CS330',
-      name: 'Image Understanding',
-      creditHours: 3,
-    },
-    {
-      id: 47,
-      code: 'CS332',
-      name: 'Computer Graphics',
-      creditHours: 3,
-    },
-    {
-      id: 48,
-      code: 'CS419',
-      name: 'Compiler Construction',
-      creditHours: 3,
-    },
-    {
-      id: 49,
-      code: 'CS477',
-      name: 'Mobile Computing',
-      creditHours: 3,
-    },
-  ]
+  ],
+}
 
-export const sections: sectionData[] = [
-  {
+export const courses: { [key: number]: Course } = {
+  1: {
+    id: 1,
+    code: 'ARB100',
+    name: 'Arabic',
+    creditHours: 3,
+  },
+  2: {
+    id: 2,
+    code: 'ENGL1001',
+    name: 'Upper-Intermediate English',
+    creditHours: 3,
+  },
+  3: {
+    id: 3,
+    code: 'ENGL1002',
+    name: 'Advanced English',
+    creditHours: 3,
+  },
+  4: {
+    id: 4,
+    code: 'GERL101B1',
+    name: 'German I B1-Track',
+    creditHours: 3,
+  },
+  5: {
+    id: 5,
+    code: 'GERL102B1',
+    name: 'German II B1-Track',
+    creditHours: 3,
+  },
+  6: {
+    id: 6,
+    code: 'GERL102B2',
+    name: 'German II B2-Track',
+    creditHours: 3,
+  },
+  7: {
+    id: 7,
+    code: 'MILS100',
+    name: 'Military Science',
+    creditHours: 3,
+  },
+  8: {
+    id: 8,
+    code: 'NE101',
+    name: 'National Education',
+    creditHours: 3,
+  },
+  9: {
+    id: 9,
+    code: 'NEE101',
+    name: 'National Education in English',
+    creditHours: 3,
+  },
+  10: {
+    id: 10,
+    code: 'GERL201B1',
+    name: 'German III B1-Track',
+    creditHours: 3,
+  },
+  11: {
+    id: 11,
+    code: 'GERL201B2',
+    name: 'German III B2-Track',
+    creditHours: 3,
+  },
+  12: {
+    id: 12,
+    code: 'GERL202B1',
+    name: 'German IV B1-Track',
+    creditHours: 3,
+  },
+  13: {
+    id: 13,
+    code: 'GERL202B2',
+    name: 'German IV B2-Track',
+    creditHours: 3,
+  },
+  14: {
+    id: 14,
+    code: 'MATH101',
+    name: 'Calculus I',
+    creditHours: 3,
+  },
+  15: {
+    id: 15,
+    code: 'MATH102',
+    name: 'Calculus II',
+    creditHours: 3,
+  },
+  16: {
+    id: 16,
+    code: 'CS116',
+    name: 'Computing Fundamentals',
+    creditHours: 3,
+  },
+  17: {
+    id: 17,
+    code: 'CS1160',
+    name: 'Computing Fundamentals Lab',
+    creditHours: 1,
+  },
+  18: {
+    id: 18,
+    code: 'CS117',
+    name: 'Object-Oriented Programming',
+    creditHours: 3,
+  },
+  19: {
+    id: 19,
+    code: 'CS1170',
+    name: 'Object-Oriented Programming Lab',
+    creditHours: 1,
+  },
+  20: {
+    id: 20,
+    code: 'CE212',
+    name: 'Digital Systems',
+    creditHours: 3,
+  },
+  21: {
+    id: 21,
+    code: 'CE2120',
+    name: 'Digital Systems Lab',
+    creditHours: 1,
+  },
+  22: {
+    id: 22,
+    code: 'EE317',
+    name: 'Linear Algebra',
+    creditHours: 3,
+  },
+  23: {
+    id: 23,
+    code: 'GERL301B1',
+    name: 'German V B1-Track',
+    creditHours: 3,
+  },
+  24: {
+    id: 24,
+    code: 'GERL301B2',
+    name: 'German V B2-Track',
+    creditHours: 3,
+  },
+  25: {
+    id: 25,
+    code: 'GERL302B1',
+    name: 'German VI B1-Track',
+    creditHours: 3,
+  },
+  26: {
+    id: 26,
+    code: 'GERL302B2',
+    name: 'German VI B2-Track',
+    creditHours: 3,
+  },
+  27: {
+    id: 27,
+    code: 'IE0121',
+    name: 'Probability and Statistics',
+    creditHours: 3,
+  },
+  28: {
+    id: 28,
+    code: 'CS201',
+    name: 'Discrete Structures',
+    creditHours: 3,
+  },
+  29: {
+    id: 29,
+    code: 'CE201',
+    name: 'Computer Architecture and Organization',
+    creditHours: 3,
+  },
+  30: {
+    id: 30,
+    code: 'CS222',
+    name: 'Theory of Algorithms',
+    creditHours: 3,
+  },
+  31: {
+    id: 31,
+    code: 'CS223',
+    name: 'Data Structures',
+    creditHours: 3,
+  },
+  32: {
+    id: 32,
+    code: 'CS264',
+    name: 'Visual Programming',
+    creditHours: 3,
+  },
+  33: {
+    id: 33,
+    code: 'CS263',
+    name: 'Database Management Systems',
+    creditHours: 3,
+  },
+  34: {
+    id: 34,
+    code: 'CS323',
+    name: 'Computational Theory',
+    creditHours: 3,
+  },
+  35: {
+    id: 35,
+    code: 'CS342',
+    name: 'Software Engineering',
+    creditHours: 3,
+  },
+  36: {
+    id: 36,
+    code: 'CE352',
+    name: 'Computer Networks',
+    creditHours: 3,
+  },
+  37: {
+    id: 37,
+    code: 'CS355',
+    name: 'Web Technologies',
+    creditHours: 3,
+  },
+  38: {
+    id: 38,
+    code: 'CS356',
+    name: 'Information Security',
+    creditHours: 3,
+  },
+  39: {
+    id: 39,
+    code: 'CE357',
+    name: 'Operating Systems',
+    creditHours: 3,
+  },
+  40: {
+    id: 40,
+    code: 'CE3570',
+    name: 'Operating Systems Lab',
+    creditHours: 1,
+  },
+  41: {
+    id: 41,
+    code: 'CS391',
+    name: 'Field Training',
+    creditHours: 0,
+  },
+  42: {
+    id: 42,
+    code: 'CS416',
+    name: 'Systems Programming',
+    creditHours: 3,
+  },
+  43: {
+    id: 43,
+    code: 'CS451',
+    name: 'Artificial Intelligence',
+    creditHours: 3,
+  },
+  44: {
+    id: 44,
+    code: 'CS491',
+    name: 'International Internship 20 weeks',
+    creditHours: 12,
+  },
+  45: {
+    id: 45,
+    code: 'CS492',
+    name: 'Senior Project',
+    creditHours: 3,
+  },
+  46: {
+    id: 46,
+    code: 'CS330',
+    name: 'Image Understanding',
+    creditHours: 3,
+  },
+  47: {
+    id: 47,
+    code: 'CS332',
+    name: 'Computer Graphics',
+    creditHours: 3,
+  },
+  48: {
+    id: 48,
+    code: 'CS419',
+    name: 'Compiler Construction',
+    creditHours: 3,
+  },
+  49: {
+    id: 49,
+    code: 'CS477',
+    name: 'Mobile Computing',
+    creditHours: 3,
+  },
+}
+
+export const sections: { [key: number]: Section } = {
+  1: {
     id: 1,
     name: 'University Requirements',
     requiredCreditHours: 16,
     courseIds: [1, 2, 3, 4, 5, 6, 7, 8, 9],
   },
-  {
+  2: {
     id: 2,
     name: 'School Requirements',
     requiredCreditHours: 21,
     courseIds: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
   },
-  {
+  3: {
     id: 3,
     name: 'Program Requirements',
     requiredCreditHours: 86,
@@ -361,97 +389,147 @@ export const sections: sectionData[] = [
       41, 42, 43, 44, 45,
     ],
   },
-  {
+  4: {
     id: 4,
     name: 'Special Program Requirements (General Track)',
     requiredCreditHours: 12,
     courseIds: [46, 47, 48, 49],
   },
-]
-
-export const initialFlowsheet: flowsheetData = {
-  id: 1,
-  name: 'Computer Science 2023/2024 - General Track',
-  yearIds: [1, 2, 3, 4, 5],
 }
 
-export const years: yearData[] = [
-  {
+export const initialFlowsheet: Flowsheet = {
+  id: 1,
+  name: 'Computer Science 2023/2024 - General Track',
+  years: {
+    1: {
+      id: 1,
+      name: 'First Year',
+      semesters: {
+        1: {
+          id: 1,
+          name: 'First',
+          courseIds: [9, 16],
+        },
+        2: {
+          id: 2,
+          name: 'Second',
+          courseIds: [],
+        },
+      },
+    },
+    2: {
+      id: 2,
+      name: 'Second Year',
+      semesters: {
+        3: {
+          id: 3,
+          name: 'First',
+          courseIds: [],
+        },
+        4: {
+          id: 4,
+          name: 'Second',
+          courseIds: [],
+        },
+      },
+    },
+    3: {
+      id: 3,
+      name: 'Third Year',
+      semesters: {
+        5: {
+          id: 5,
+          name: 'First',
+          courseIds: [],
+        },
+        6: {
+          id: 6,
+          name: 'Second',
+          courseIds: [],
+        },
+      },
+    },
+    4: {
+      id: 4,
+      name: 'Fourth Year',
+      semesters: {
+        7: {
+          id: 7,
+          name: 'First',
+          courseIds: [],
+        },
+        8: {
+          id: 8,
+          name: 'Second',
+          courseIds: [],
+        },
+      },
+    },
+  },
+}
+
+export const years: { [key: number]: Year } = {
+  1: {
     id: 1,
-    name: 'First',
+    name: 'First Year',
     semesterIds: [1, 2],
   },
-  {
+  2: {
     id: 2,
-    name: 'First',
+    name: 'Second Year',
     semesterIds: [3, 4],
   },
-  {
+  3: {
     id: 3,
-    name: 'First',
+    name: 'Third Year',
     semesterIds: [5, 6],
   },
-  {
+  4: {
     id: 4,
-    name: 'First',
+    name: 'Fourth Year',
     semesterIds: [7, 8],
   },
-  {
-    id: 5,
-    name: 'First',
-    semesterIds: [9, 10],
-  },
-]
+}
 
-export const semesters: semesterData[] = [
-  {
+export const semesters: { [key: number]: Semester } = {
+  1: {
     id: 1,
     name: 'First',
-    courseIds: [],
+    courseIds: [9, 16],
   },
-  {
+  2: {
     id: 2,
     name: 'Second',
     courseIds: [],
   },
-  {
+  3: {
     id: 3,
     name: 'First',
     courseIds: [],
   },
-  {
+  4: {
     id: 4,
     name: 'Second',
     courseIds: [],
   },
-  {
+  5: {
     id: 5,
     name: 'First',
     courseIds: [],
   },
-  {
+  6: {
     id: 6,
     name: 'Second',
     courseIds: [],
   },
-  {
+  7: {
     id: 7,
     name: 'First',
     courseIds: [],
   },
-  {
+  8: {
     id: 8,
     name: 'Second',
     courseIds: [],
   },
-  {
-    id: 9,
-    name: 'First',
-    courseIds: [],
-  },
-  {
-    id: 10,
-    name: 'Second',
-    courseIds: [],
-  },
-]
+}
